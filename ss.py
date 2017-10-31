69e97f5a34eef61e5fb048fc161b657c71414433
Gunjan Chandani <gunjankaramchandani@gmail.com>
2017-10-31 19:49:55 +0530

    Color profile conversion added

--- ss.py
+++ ss.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-
 import os
 import subprocess
 import sys
+import PIL.Image
+import PIL.ImageCms
+import tempfile
 
 def ensure_dir(file_path):
     directory = os.path.dirname(file_path)
@@ -17,11 +19,22 @@ def save_screencap(cap, filename):
     with open(filename, 'wb') as f:
         f.write(cap)
 
+def convert_profile(filename):
+    img = PIL.Image.open(filename)
+    icc = tempfile.mkstemp(suffix='.icc')[1]
+    if 'icc_profile' in img.info:
+        with open(icc, 'wb') as f:
+            f.write(img.info['icc_profile'])
+        srgb = PIL.ImageCms.createProfile('sRGB')
+        img = PIL.ImageCms.profileToProfile(img, icc, srgb)
+    img.save(filename)
+
 if __name__ == "__main__":
     short_filename = sys.argv[1]
     filename = 'img/' + short_filename + '.png'
     ensure_dir(filename)
     save_screencap(screencap(), filename)
+    convert_profile(filename)
     if sys.argv[2] != '-s':
         print("Insert image into doc with:")
         print(".. image:: /img/" + short_filename + ".*")
