49bb6e090b34135953b3d1ecc2f5324716256d68
Ankita Saxena <ankitasonu24@gmail.com>
2018-01-13 00:07:13 +0530

    added script to copy checks to proselint and run checks

---
+++ style-test.py
@@ -0,0 +1,68 @@
+import os
+import glob
+import shutil
+import proselint
+
+def add_checks(): 	   
+
+	dest = os.path.dirname(proselint.__file__)
+	dest_prc = dest + '/.proselintrc'
+	dest = dest + '/checks/style-guide'
+
+	if os.path.exists(dest):
+	   pass
+	else:
+	   src ='./style-guide'       
+	   shutil.copytree(src, dest)
+
+	with open("style-test.txt", "r") as file:
+	   tests = file.readlines()
+
+	with open(dest_prc, "r") as in_file:
+	    buf = in_file.readlines()
+
+	pos = len(buf)-2
+
+	for index, line in enumerate(buf):
+	    if line.startswith("        \"style-guide."):
+	       pos = index
+	       break      
+
+	with open(dest_prc, "w") as out_file:
+	    for index, line in enumerate(buf):
+	        if index == pos-1 and not line.endswith(",\n"):
+	           line = line[0:line.rfind("\n")] + ",\n"
+	        if index < pos:
+	           out_file.write(line)
+	    for test in tests:
+	       out_file.write(test)           	    
+	    out_file.write("    }\n")
+	    out_file.write("}")
+
+def run_checks():
+
+	file_path = os.path.realpath(__file__)
+	search_path = file_path[0:file_path.rfind('/')]
+	for filename in glob.glob(os.path.join(search_path, '*.rst')):
+	    txt=open(filename,'r')
+	    errors = proselint.tools.lint(txt)
+	    err_list = []
+	    for e in errors:
+	        check = "check: %s, " %e[0] 
+	        msg = "message: %s, " %e[1]
+	        line = "line: %d, " %(1 + e[2])
+	        column = "column: %d, " %(1 + e[3])
+	        start = "start: %d, " %(1 + e[4])
+	        end = "end: %d, " %(1 + e[5])
+	        extent = "extent: %d, " %e[6]
+	        severity = "severity: %s, " %e[7]
+	        replace = "replacements: %s " %e[8]
+	        err_str = check + msg + line + column + start + end + extent + severity + replace
+	        err_list.append(err_str)    
+	    print(filename)
+	    for e in err_list:
+	    	print(e)
+	    	print('\n') 
+
+add_checks()
+run_checks()
