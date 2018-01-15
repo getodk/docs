706eb38204292ff9afecf1100ff6890d64aa9c5f
Ankita Saxena <ankitasonu24@gmail.com>
2018-01-16 00:51:54 +0530

    improve check for straight quotes

--- style-guide/check-quotes.py
+++ style-guide/check-quotes.py
@@ -9,6 +9,6 @@ def check_quotes(text):
     """Avoid quote marks."""
     err = "style-guide.check-quotes"
     msg = "Avoid using quote marks."
-    regex = "\"[^\"]*\""
+    regex = "\W\"[a-zA-z0-9\s]*\"\W"
 
     return existence_check(text, [regex], err, msg, require_padding=False)
