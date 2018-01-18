a008595b6f8ab06504291a7e07df87abbef8649e
Ankita Saxena <ankitasonu24@gmail.com>
2018-01-19 02:48:38 +0530

    add check for section labels

--- style-guide/extra.py
+++ style-guide/extra.py
@@ -22,6 +22,7 @@ def check_quotes(text):
 
     return errors
 
+
 def check_curlyquotes(text):
     """Check curly quotes"""
     err = "style-guide.check-curlyquote"
@@ -40,6 +41,31 @@ def check_curlyquotes(text):
 
     return errors
 
+
+def check_label(text):
+    """Check section label"""
+    err = "style-guide.check-label"
+    msg = "Add a section label if required."
+    regex = r"(.*\n)(( )*\n)(.+\n)(([=\-~\"\']){3,})"
+
+    errors = []
+
+    for m in re.finditer(regex, text):
+        label = m.group(1)
+        start = m.start()+1
+        end = m.end()
+        (row, col) = line_and_column(text, start)
+        row = row + 2
+        col = 0
+        extent = m.end()-m.start()
+        catches = tuple(re.finditer(r"\.\. _", label))
+        if not len(catches):
+           errors += [(err, msg, row, col, start, end,
+                           extent, "warning", "None")]
+
+    return errors       
+
+
 def line_and_column(text, position):
     """Return the line number and column of a position in a string."""
     position_counter = 0
@@ -49,10 +75,11 @@ def line_and_column(text, position):
         else:
             position_counter += len(line)    
 
+
 def check(text):
     """Run checks for quote marks and section labels"""
     text = text.read()
     error = []
-    errors = check_quotes(text) + check_curlyquotes(text)    
+    errors = check_quotes(text) + check_curlyquotes(text) + check_label(text)  
     errors = sorted(errors, key=lambda e: (e[2], e[3]))
     return errors
