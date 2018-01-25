944ed192c729eb326020cdfe9219651344932b23
Ankita Saxena <ankitasonu24@gmail.com>
2018-01-25 05:34:54 +0530

    fix message for personal pronoun

--- style-guide/personal-pronoun.py
+++ style-guide/personal-pronoun.py
@@ -8,7 +8,8 @@ from proselint.tools import existence_check, memoize
 def check_pronoun(text):
     """Check third-person personal pronouns."""
     err = "style-guide.personal-pronoun"
-    msg = "Avoid using third-person personal pronouns like \"he\", \"she\", \"them\"."
+    msg = "Avoid using third-person personal pronouns like \"he\", \"she\". "
+    msg = msg + "In case of absolute need, prefer using \"they\"."
 
     list = [
         "he",
