3f84a88c3fa0627b31e2db956ca105d3d128457f
adammichaelwood <adam.michael.wood@gmail.com>
2018-01-26 15:36:33 -0800

    ss vs details lables working for list items

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -15,6 +15,7 @@ $(function(){
 	togl = "<span class='toggler hiding' data-toggle='" + id + "'></span><br>";
 	$(this).before(togl);
 	$(this).addClass('hidden');
+	$(this).has("img:only-child").addClass('screenshot-only');
     });
 
     // Add class to list items that include togglers
@@ -26,20 +27,35 @@ $(function(){
     // Define the toggler labels
     hiding_label = "[<span class='toggle-sign'>&plus;</span>] show details";
     showing_label = "[<span class='toggle-sign'>&minus;</span>] hide details";
+    ss_hiding_label = "[<span class='toggle-sign'>&plus;</span>] show screenshot";
+    ss_showing_label = "[<span class='toggle-sign'>&minus;</span>] hide screenshot";
+
 
     // Add labels and onclick function to togglers
     $(".toggler").each(function(index){
-	$(this).html(hiding_label);
+	if ($(this).siblings().hasClass('screenshot-only')) {
+	    $(this).html(ss_hiding_label);
+	} else {
+	    $(this).html(hiding_label);
+	}
 	$(this).click(function(e){
 	    toggle_image = $('#' + $(this).attr('data-toggle'));
 	    if ($(this).hasClass('hiding')) {
-		$(this).html(showing_label);
 		$(this).removeClass('hiding');
 		toggle_image.removeClass("hidden");
+		if ($(this).siblings().hasClass('screenshot-only')) {
+		    $(this).html(ss_showing_label);
+		} else {
+		    $(this).html(showing_label);
+		}
 	    } else {
-		$(this).html(hiding_label);
 		$(this).addClass('hiding');
 		toggle_image.addClass("hidden");
+		if ($(this).siblings().hasClass('screenshot-only')) {
+		    $(this).html(ss_hiding_label);
+		} else {
+		    $(this).html(hiding_label);
+		}
 	    }
 	})
     });    
