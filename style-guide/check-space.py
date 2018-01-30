421b5faaa54556e6da27885f3d64a1dad7268a71
Ankita Saxena <ankitasonu24@gmail.com>
2018-01-30 21:24:34 +0530

    improved check for spacing after period

--- style-guide/check-space.py
+++ style-guide/check-space.py
@@ -9,7 +9,7 @@ def check_space(text):
     """Use single space after sentence end."""
     err = "style-guide.check-space"
     msg = "Missing space after period."
-    regex = "[\.\?!][A-Z]"
+    regex = "[\.\?!][A-Z]{2,}"
 
     return existence_check(text, [regex], err, msg, ignore_case=False, 
     	                   require_padding=False)
