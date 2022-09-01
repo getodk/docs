80134c4b9c7f98caeee7539e766a1b5b95a983bf
Yaw Anokwa <yanokwa@gmail.com>
2022-08-31 17:08:30 -0700

    Update to furo theme

---
+++ docs/conf.py
@@ -0,0 +1,81 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import platform
+
+# -- Project information -----------------------------------------------------
+
+project = 'ODK'
+copyright = '2022 Get ODK Inc. Licensed under CC BY 4.0.'
+author = 'ODK'
+
+# The full version, including alpha/beta/rc tags
+# release = 'v1.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [ 'sphinxcontrib.spelling',
+    'sphinx_design'
+]
+
+# enchant requires an Intel chip
+if 'arm' in platform.processor():
+    extensions.remove('sphinxcontrib.spelling')
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
+
+html_css_files = [
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css',
+    'css/custom.css',
+]
+
+html_js_files = [
+    ('https://thirtyfive-england.getodk.org/script.js', {'defer': 'defer', 'data-site': 'PBSTMJFG'})
+]
+
+html_theme_options = {
+    'light_css_variables': {
+        'color-brand-primary': '#009ECC',
+        'color-brand-content': '#009ECC',
+    },
+    'sidebar_hide_name': True,
+}
+html_show_sphinx = True
+
+html_favicon = '_static/img/odk-favicon.ico'
+html_logo = '_static/img/odk-logo.svg'
